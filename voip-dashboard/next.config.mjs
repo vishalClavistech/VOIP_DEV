382570233809027b52c1b88f730690e18df02a60
Vishal Vaibhaw <vishal.clavis@outlook.com>
2025-09-11 10:24:21 +0530

    change for build and config

--- voip-dashboard/next.config.mjs
+++ voip-dashboard/next.config.mjs
@@ -1,5 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',   // enables static export build
+  assetPrefix: '/voip/',
+  images: {
+    unoptimized: true // needed if you use <Image> component
+  },
+  basePath: '/voip',
   reactStrictMode: true,
   experimental: {
     instrumentationHook: false
